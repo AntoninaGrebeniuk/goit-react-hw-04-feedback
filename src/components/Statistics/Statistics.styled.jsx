23335b8d88d00e0fc99edcf20e2f66eee2ac6de7
Antonina Grebeniuk <rebel.t2808@gmail.com>
2023-06-06 17:07:14 +0300

    Update Statistics.styled.jsx

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -6,12 +6,10 @@ export const StatisticList = styled.ul`
 
 export const StatisticItem = styled.li`
   display: flex;
-  /* justify-content: center; */
   align-items: center;
   font-size: 32px;
 `;
 
 export const StatisticText = styled.span`
-  /* width: 180px; */
   margin-right: 15px;
 `;
